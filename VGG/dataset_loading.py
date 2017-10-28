848f359eb47664f4e326399070931f6e6baa1971
Shreya Rajpal <shreya.rajpal@gmail.com>
2017-10-28 08:32:47 -0400

    Added Image Transforms

--- VGG/dataset_loading.py
+++ VGG/dataset_loading.py
@@ -1,26 +1,35 @@
 from PIL import Image
-from torchvision.transforms import ToTensor, Normalize, Resize, RandomCrop
+from torchvision.transforms import ToTensor, Normalize, Resize, RandomCrop, Compose
 
-class SiameseNetworkDataset(Dataset):
+class SiameseNetworkDataset:
     
     def __init__(self,imageFile,transform=None):
         self.imageFile = imageFile    
-        self.transform = transform
+	# self.totensor = ToTensor()
+	self.resize = Resize(224)
+	#self.normalize = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+	#self.randomcrop = RandomCrop(224)
+	self.transforms = Compose([RandomCrop(224), ToTensor(),
+		Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         
-    def __getitem__(self,index):
+    def __getitem__(self):
         with open(self.imageFile) as f:
             for line in f:
 
                 im1, im2, label = line.rstrip().split()
-                im1 = ToTensor(Image.open(im1))
-                im2 = ToTensor(Image.open(im2))
+                im1 = Image.open(im1)
+                im2 = Image.open(im2)
+		
+		if min(im1[0,0,:].size()[0], im1[0,:,0].size()[0]) < 224:
+			im1 =  Resize.__call__(im1)
+		if min(im2[0,0,:].size()[0], im2[0,:,0].size()[0]) < 224:
+			im2 = Resize.__call__(im2)
+		
+		im1 = self.transforms.__call__(im1)
+		im2 = self.transforms.__call__(im2)
 
-                print im1.size
-                
-                
-
-        
-                yield img0, img1 , torch.from_numpy(np.array([int(img1_tuple[1]!=img0_tuple[1])],dtype=np.float32))
+     		yield im1, im2, int(label)   	
+                # yield img0, img1 , torch.from_numpy(np.array([int(img1_tuple[1]!=img0_tuple[1])],dtype=np.float32))
     
-    def __len__(self):
-        return len(self.imageFile.imgs)
+    #def __len__(self):
+    #    return len(self.imageFile.imgs)
---
+++ VGG/temp.py
@@ -0,0 +1,8 @@
+from torchvision.transforms import ToTensor
+from dataset_loading import SiameseNetworkDataset
+
+x = SiameseNetworkDataset("/data/srajpal2/AmazonDataset/test_train.txt")
+
+for im1, im2 in x.__getitem__():
+	print im1.size(), im2.size()
+
