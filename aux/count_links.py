15560ce49a455c526ecc390d967ad0e3978c99ac
Shreya Rajpal <shreya.rajpal@gmail.com>
2017-11-01 13:59:08 -0500

    Count co-purchasing links

---
+++ aux/count_links.py
@@ -0,0 +1,14 @@
+import json
+
+compatibility_links = 0
+similarity_links = 0
+all_links = 0
+
+with open("/data/srajpal2/AmazonDataset/updated_categories_meta_tbs.json") as f:
+	for line in f:
+		info = json.loads(line.rstrip())
+		compatibility_links += len(info["related"]["compatible"])
+		similarity_links += len(info["related"]["similar"])
+		all_links += len(info["related"]["compatible"])+len(info["related"]["similar"])
+
+print compatibility_links, similarity_links, all_links
