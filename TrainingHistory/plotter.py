44275ce078574cd9b024442ad5c195eb32be0f6f
Shreya Rajpal <shreya.rajpal@gmail.com>
2017-12-18 22:20:08 -0600

    Partially done plotter.py

--- TrainingHistory/plotter.py
+++ TrainingHistory/plotter.py
@@ -54,29 +54,72 @@ class LearningRatePlot:
 		plt.show()
 
 
+class GeneralPurposePlotter:
+
+	def __init__(self, opts, training_history_dict, val_history_dict=None,
+			training_history_smoothing=10, val_history_smoothing=1,
+			train_val_same_plot=True, all_versions_same_plot=True):
+		num_opts = len(opts)
+
+		fig = plt.figure()
+		ax = fig.add_subplot(1,1,1)
+
+		lines = {}
+		for o in opts:
+			training_history = np.array(training_history_dict[o])
+			moving_avg_loss = self.running_mean(training_history[:,1], training_history_smoothing)
+			# print moving_avg_loss.shape, training_history[10:,:].shape
+			lines["%s: Train" % (o)] ,= ax.plot(training_history[training_history_smoothing:,0], moving_avg_loss, label="%s: Train" % (o))
+
+		plt.legend(handles=lines.values())
+		# ax.set_xscale('log')
+		plt.show()
+
+
+	def running_mean(self, x, N):
+		# cumsum = np.cumsum(np.insert(x, 0, 0))
+		cumsum = np.cumsum(x)
+		return (cumsum[N:] - cumsum[:-N]) / float(N)
+
+
+
+
+
 class TrainingValHistory:
 
+
 	def __init__(self, versions):
+		training_hist_dict = {}
+		val_history_dict = {}
 		for v in versions:
 			info = self.getInfo(v)
-			training_iter, training_loss = self.readTrainingLoss(v)
-			
-			
+			training_hist = self.readTrainingHist(v)
+			val_hist = self.readValHist(v)
+			training_hist_dict[info] = training_hist
+			val_history_dict[info] = val_hist
+		GeneralPurposePlotter(training_hist_dict.keys(), training_hist_dict, val_history_dict, training_history_smoothing=100)
 
 	def getInfo(self, v):
-		with open("V%d/Similarity/info.txt") as f:
-			info = [x.rstrip().split(': ') for x in f.readlines()]
+		with open("V%d/Similarity/info.txt" % v) as f:
+			info = [x.rstrip().split(': ')[1] for x in f.readlines()]
 		return info[3]
 
-	def readTrainingLoss(self):
+	def readTrainingHist(self,v):
 		info = []
-		with open("V%d/Similarity/training_loss.txt") as f:
+		with open("V%d/Similarity/training_loss.txt" % v) as f:
 			for line in f:
-					
+				iteratn, loss = line.rstrip().split(', ')[:2]
+				info.append([int(iteratn), float(loss)])
+		return info
+
+	def readValHist(self,v):
+		info = []
+		with open("V%d/Similarity/val_loss.txt" % v) as f:
+			for line in f:
+				iteratn, loss = line.rstrip().split(', ')[:2]
+				info.append([int(iteratn), float(loss)])
+		return info
 
 
-
-	
-		
-
-LearningRatePlot(range(16, 16+28))
+# LearningRatePlot(range(16, 16+28))
+TrainingValHistory([16,23])
